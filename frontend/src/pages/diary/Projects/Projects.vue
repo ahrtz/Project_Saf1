4551a309d1d8120208e095d47c40cc1003b2acb5
ahrtz <ahrtzzinn@gmail.com>
2020-07-24 11:32:36 +0900

    add 더미데이터 조금

--- frontend/src/pages/diary/Projects/Projects.vue
+++ frontend/src/pages/diary/Projects/Projects.vue
@@ -1,13 +1,18 @@
 <template>
     <div>
         <h2>프로젝트 카드 뷰</h2>
-       
+       {{projects}}
     </div>
 </template>
 
 <script>
 export default {
-    name:'Projects'
+    name:'Projects',
+    data(){
+        return{
+            projects:['1번 다이어리','1번 다이어리','1번 다이어리','1번 다이어리','1번 다이어리']
+        }
+    }
 }
 </script>
 
