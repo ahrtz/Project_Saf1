9039184cb4fdd56b87f45c64066a7b36ffdb04b7
박진수 <ahrtzzinn@gmail.com>
2020-07-28 14:23:52 +0900

    [ADD]블로그 추가

--- frontend/src/pages/user/Scrap.vue
+++ frontend/src/pages/user/Scrap.vue
@@ -1,7 +1,17 @@
 <template>
   <div>
-      <h2>스크랩 페이지</h2>
-      <user-sidebar/>
+    <v-container>
+        <v-row>
+            <v-col cols="9">
+              <h2>스크랩 페이지</h2>
+
+            </v-col>
+            <v-col cols="3">
+                <user-sidebar/>
+            </v-col>
+        </v-row>
+
+    </v-container>
   </div>
 </template>
 
