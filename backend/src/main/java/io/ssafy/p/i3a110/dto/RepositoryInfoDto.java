625593655e7794a044a994ea6e7fabea36b94aca
Jihwan Oh <ojhoshlmw@gmail.com>
2020-07-28 17:56:09 +0900

    [Add] CommitController 관련 추가, CommitListDto 변수 추가

--- backend/src/main/java/io/ssafy/p/i3a110/dto/RepositoryInfoDto.java
+++ backend/src/main/java/io/ssafy/p/i3a110/dto/RepositoryInfoDto.java
@@ -1,45 +1,17 @@
 package io.ssafy.p.i3a110.dto;
 
+import lombok.Data;
 
+@Data
 public class RepositoryInfoDto {
 	private String repoName;
 	private String owner;
 	private String url;
 	private Boolean isPrivate;
-	
 	public RepositoryInfoDto(String repoName, String owner, String url, Boolean isPrivate) {
 		this.repoName = repoName;
 		this.owner = owner;
 		this.url = url;
 		this.isPrivate = isPrivate;
-	}
-	public String getRepoName() {
-		return repoName;
-	}
-	public void setRepoName(String repoName) {
-		this.repoName = repoName;
-	}
-	public String getOwner() {
-		return owner;
-	}
-	public void setOwner(String owner) {
-		this.owner = owner;
-	}
-	public String getUrl() {
-		return url;
-	}
-	public void setUrl(String url) {
-		this.url = url;
-	}
-	public Boolean getIsPrivate() {
-		return isPrivate;
-	}
-	public void setIsPrivate(Boolean isPrivate) {
-		this.isPrivate = isPrivate;
-	}
-	@Override
-	public String toString() {
-		return "RepositoryInfo [repoName=" + repoName + ", owner=" + owner + ", url=" + url + ", isPrivate=" + isPrivate
-				+ "]";
 	}
 }
