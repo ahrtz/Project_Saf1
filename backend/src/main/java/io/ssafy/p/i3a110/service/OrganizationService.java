6d5218f83512b16f572970e7c8e75f46dcef9b20
Jihwan Oh <ojhoshlmw@gmail.com>
2020-07-30 15:08:46 +0900

    [Fix] GitController 날짜검색 추가,[S03P12A110-74]

---
+++ backend/src/main/java/io/ssafy/p/i3a110/service/OrganizationService.java
@@ -0,0 +1,17 @@
+package io.ssafy.p.i3a110.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import io.ssafy.p.i3a110.dao.HealthDao;
+import io.ssafy.p.i3a110.dto.HealthDto;
+
+@Service
+public class OrganizationService {
+	@Autowired
+	private HealthDao healthDao;
+	
+	public HealthDto getHealth() {
+		return healthDao.getHealth();
+	}
+}
