a3774825a92da6c0a19da044b36fa7ac8d378ff6
Jihwan Oh <ojhoshlmw@gmail.com>
2020-08-03 17:56:37 +0900

    [Fix]TagDto 일부 수정

--- backend/src/main/java/io/ssafy/p/i3a110/config/WebMvcConfigurerImpl.java
+++ backend/src/main/java/io/ssafy/p/i3a110/config/WebMvcConfigurerImpl.java
@@ -2,7 +2,11 @@ package io.ssafy.p.i3a110.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import ch.qos.logback.core.recovery.ResilientFileOutputStream;
+import io.ssafy.p.i3a110.interceptor.LoginCheckInterceptor;
 
 @Configuration
 public class WebMvcConfigurerImpl implements WebMvcConfigurer{
@@ -10,4 +14,10 @@ public class WebMvcConfigurerImpl implements WebMvcConfigurer{
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**");
     }
+
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		registry.addInterceptor(new LoginCheckInterceptor());
+	}
+    
 }
