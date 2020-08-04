74b2f6128b37a95d345ef82d759e136f4e5783f9
chaeeun kim <fairy037@naver.com>
2020-08-04 09:28:37 +0900

    [Fix] google oauth2 추가해서 일어나는 버그 수정을 위해 코드 원복

--- backend/src/main/java/io/ssafy/p/i3a110/config/SecurityConfig.java
+++ backend/src/main/java/io/ssafy/p/i3a110/config/SecurityConfig.java
@@ -1,32 +1,32 @@
-package io.ssafy.p.i3a110.config;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-
-@Configuration
-@EnableWebSecurity
-@PropertySource("classpath:application.properties")
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
-
-    @Autowired
-    private LoginSuccessHandler loginSuccessHandler;
-
-    @Autowired
-    private LoginFailureHandler loginFailureHandler;
-
-    @Override
-    public void configure(HttpSecurity httpSecurity) throws Exception {
-        httpSecurity
-                .antMatcher("/**").authorizeRequests()
-                .antMatchers("/google").authenticated()
-                .anyRequest().permitAll()
-                .and()
-                .oauth2Login()
-                .successHandler(loginSuccessHandler)
-                .failureHandler(loginFailureHandler);
-    }
-}
+//package io.ssafy.p.i3a110.config;
+//
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.context.annotation.PropertySource;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+//
+//@Configuration
+//@EnableWebSecurity
+//@PropertySource("classpath:application.properties")
+//public class SecurityConfig extends WebSecurityConfigurerAdapter {
+//
+//    @Autowired
+//    private LoginSuccessHandler loginSuccessHandler;
+//
+//    @Autowired
+//    private LoginFailureHandler loginFailureHandler;
+//
+//    @Override
+//    public void configure(HttpSecurity httpSecurity) throws Exception {
+//        httpSecurity
+//                .antMatcher("/**").authorizeRequests()
+//                .antMatchers("/google").authenticated()
+//                .anyRequest().permitAll()
+//                .and()
+//                .oauth2Login()
+//                .successHandler(loginSuccessHandler)
+//                .failureHandler(loginFailureHandler);
+//    }
+//}
