6ce7f07639e0b0a6ce44763e9c15095b23a2a7b5
홍창경 <dominicong@naver.com>
2020-08-04 15:11:02 +0900

    [Add] git profile 컴포넌트 추가, S03P13A110-16

---
+++ frontend/src/component/GitProfile.vue
@@ -0,0 +1,40 @@
+<template>
+<div>
+<div class="profile" v-for="(j,key) in 4" :key="key" >
+    <div class="profile-tile" :class="{'profile-tile-black': j * i % 2 == 1, 'profile-tile-darkgrey' : (i%2 == 0)&(j%2)==1 }" v-for="(i, index) in 7" :key="index">
+        
+    </div>  
+</div>
+</div>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+    name : 'GitProfile'
+}
+</script>
+<style>
+.profile{
+    width:100%;
+    display:flex;
+    flex-direction: row;
+}
+
+.profile-tile {
+    width : 40px;
+    height : 40px;
+    background-color : rgb(51, 50, 50);
+    border-radius:  4px;
+    margin : 1px;
+}
+
+.profile-tile-black {
+    background-color : black;
+}
+
+.profile-tile-darkgrey {
+    background-color:darkgrey;
+}
+</style>
--- frontend/src/component/Status.vue
+++ frontend/src/component/Status.vue
@@ -1,18 +1,30 @@
 <template>
-  <div>
+  <div class="pa-2 status-board">
       <!-- 스테이터스를 넣어보자
       <br>
       이건 메인이랑 블로그 디테일이랑 다이어리 디테일에서 쓰일 겁니다. -->
-      <img style="width: 100%" src="/static/images/grass.png" />
+      <profile class="mr-2" v-for="(i,index) in 3" :key="index">
+      </profile>
+      
+      <!-- <img style="width: 100%" src="/static/images/grass.png" /> -->
   </div>
 </template>
 
 <script>
+import profile from './GitProfile.vue'
 export default {
 name:'status',
+components : {
+  profile,
+}
 }
 </script>
 
 <style>
-
+.status-board {
+  display:flex;
+  flex-direction: row;
+  flex-grow: 0;
+  justify-content:center;
+}
 </style>
