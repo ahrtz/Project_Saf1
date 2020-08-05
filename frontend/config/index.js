41c3615d3c1882953c2b7fcc39144b6a8a4c4594
홍창경 <dominicong@naver.com>
2020-08-05 13:23:52 +0900

    [Infra]index.js Frontedn 서버, 로컬 구분 편하도록 line 추가

--- frontend/config/index.js
+++ frontend/config/index.js
@@ -31,7 +31,8 @@ module.exports = {
     },
 
     // Various Dev Server settings
-    host: 'localhost', // can be overwritten by process.env.HOST
+    host: 'localhost', // 로컬용 can be overwritten by process.env.HOST
+    //host: '172.26.1.240', // 서버용(EC2)
     port: 8080, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
     autoOpenBrowser: false,
     errorOverlay: true,
