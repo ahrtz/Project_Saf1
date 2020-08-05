6794f675790e49be8cae1370164ba4b0ee9e2d22
홍창경 <dominicong@naver.com>
2020-08-05 19:13:51 +0900

    [Add] Group 페이지 조회 추가,[S03P13A110-21]

--- frontend/src/pages/Group/TempGroupMain.vue
+++ frontend/src/pages/Group/TempGroupMain.vue
@@ -11,7 +11,7 @@
     </v-card-title>
     <v-data-table
       :headers="headers"
-      :items="groups"
+      :items="group_list"
       :search="search"
     ></v-data-table>
   </v-card>
@@ -21,18 +21,46 @@
   export default {
     data () {
       return {
+        uid : null, 
         search: '',
         headers: [
-          {
-            text: 'Name',
-            align: 'start',
-            filterable: false,
-            value: 'name',
-          },
-          { text: 'Leader', value: 'leader' },
+          { text: 'Name', align: 'start', value: 'name',},
           { text: 'Members', value: 'members' },
+          { text: 'Leader', value: 'leader',  filterable: false },
         ],
-        groups : [],
+        groups : [],  //api에 대응하는 group
+        group_list : [] //Table에 띄워줄 data 포멧.
+      }
+    },
+    async created(){
+      this.uid=this.$store.state.user.id
+      console.log('CKCK' + this.uid)
+      this.getGroup();
+    },
+    methods:{
+      async getGroup(){
+        try{
+          let temp = await this.$api.getGroupList()
+          this.groups = temp
+
+          this.setDataTable()
+          console.log('CKCK:: Group 가져오기 성공')
+        }
+        catch(e)
+        {
+          console.log(e)
+        }
+      },
+      setDataTable(){
+        console.log('CKCK:: Group 정보' + this.groups.length)
+        for(var i =0; i<this.groups.length; i++)
+        {
+          var obj = {}
+          obj['name'] = this.groups[i].name;
+          obj['members'] = this.groups[i].mCnt;
+          obj['leader'] = this.groups[i].lName;
+          this.group_list.push(obj);
+        }
       }
     },
   }
