44ff72f250d3e6766eaa847c75aa68dc72550ec9
Jihwan Oh <ojhoshlmw@gmail.com>
2020-08-06 11:35:02 +0900

    [Add] FollowController 설계, [S03P13A110-30]

---
+++ backend/src/main/java/io/ssafy/p/i3a110/dao/FollowDao.java
@@ -0,0 +1,22 @@
+package io.ssafy.p.i3a110.dao;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+
+import io.ssafy.p.i3a110.dto.FollowDto;
+import io.ssafy.p.i3a110.dto.HealthDto;
+@Mapper
+public interface FollowDao {
+	public HealthDto getHealth();
+
+	public List<Integer> getFollower(int uid);
+
+	public List<Integer> getFollowing(int uid);
+
+	public FollowDto checkFollow(int userFrom, int userTo);
+
+	public void updateFollow(FollowDto follow);
+
+	public void createFollow(FollowDto follow);
+}
