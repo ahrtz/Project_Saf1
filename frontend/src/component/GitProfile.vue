107279bccebe7eea25845c6e70c376bfeb376408
chaeeun kim <fairy037@naver.com>
2020-08-10 12:18:00 +0900

    [Fix] front UI디자인 수정 중... status, main layout 등

--- frontend/src/component/GitProfile.vue
+++ frontend/src/component/GitProfile.vue
@@ -1,40 +1,46 @@
+<!--쓰이는곳 없음-->
 <template>
-<div>
-<div class="profile" v-for="(j,key) in 4" :key="key" >
-    <div class="profile-tile" :class="{'profile-tile-black': j * i % 2 == 1, 'profile-tile-darkgrey' : (i%2 == 0)&(j%2)==1 }" v-for="(i, index) in 7" :key="index">
-        
-    </div>  
-</div>
-</div>
+  <div>
+    <!-- <div class="profile" v-for="(j,key) in 4" :key="key">
+      <div
+        class="profile-tile"
+        :class="{'profile-tile-black': j * i % 2 == 1, 'profile-tile-darkgrey' : (i%2 == 0)&(j%2)==1 }"
+        v-for="(i, index) in 7"
+        :key="index"
+      ></div>
+    </div>-->
+  </div>
 </template>
 
 <script>
-import axios from 'axios'
+import axios from 'axios';
 
 export default {
-    name : 'GitProfile'
-}
+  name: 'GitProfile',
+  
+};
 </script>
 <style>
-.profile{
-    width:100%;
-    display:flex;
-    flex-direction: row;
+.profile {
+  width: 100%;
+  display: flex;
+  flex-direction: row;
 }
 
 .profile-tile {
-    width : 40px;
-    height : 40px;
-    background-color : rgb(51, 50, 50);
-    border-radius:  4px;
-    margin : 1px;
+  width: 35px;
+  height: 35px;
+  border-radius: 4px;
+  margin: 2px;
 }
 
 .profile-tile-black {
-    background-color : black;
+  background-color: black;
 }
 
 .profile-tile-darkgrey {
-    background-color:darkgrey;
+  background-color: darkgrey;
 }
+
+
 </style>
