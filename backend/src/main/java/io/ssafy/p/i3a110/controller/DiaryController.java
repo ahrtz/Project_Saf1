182a7e15d95ea63ba59eeda28fc16733cb2cb1a4
chaeeun kim <fairy037@naver.com>
2020-08-11 11:32:51 +0900

    [Add] diary 생성, 수정 image file multi data 추가

--- backend/src/main/java/io/ssafy/p/i3a110/controller/DiaryController.java
+++ backend/src/main/java/io/ssafy/p/i3a110/controller/DiaryController.java
@@ -1,20 +1,25 @@
 package io.ssafy.p.i3a110.controller;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 
 import javax.servlet.http.HttpSession;
 
+import org.apache.commons.io.FilenameUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import io.ssafy.p.i3a110.dto.DiaryDto;
 import io.ssafy.p.i3a110.dto.UserDto;
@@ -22,13 +27,19 @@ import io.ssafy.p.i3a110.interceptor.Auth;
 import io.ssafy.p.i3a110.service.DiaryService;
 import io.ssafy.p.i3a110.service.UserService;
 import io.swagger.annotations.ApiOperation;
+import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 public class DiaryController {
+    private static final String BE_BASE_URL = "http://i3a110.p.ssafy.io:3000";
+
     @Autowired
     private DiaryService diaryService;
     @Autowired
     private UserService userService;
+
+    @Value("${application.staticPath}")
+    private String staticPath;
 
     // 전체 Diary 검색 (type - 0: Blog, 1: Project, 2 or Other: All)
     @PostMapping("/diaries/{uid}")
@@ -51,10 +62,45 @@ public class DiaryController {
     @Auth
     @PostMapping("/diaries")
     @ApiOperation(value = "다이어리 생성")
-    public Object createDiary(HttpSession session, @RequestBody DiaryDto diary) {
-    	String email = (String) session.getAttribute("email");
+    public Object createDiary(
+                            HttpSession httpSession,
+                            @RequestParam(required = false) MultipartFile file,
+                              @RequestParam int uid,
+                              @RequestParam String gitName,
+                              @RequestParam String title,
+                              @RequestParam String intro,
+                              @RequestParam String img,
+                              @RequestParam String gitUrl,
+                              @RequestParam int isProj,
+                              @RequestParam Date sDate,
+                              @RequestParam Date eDate) throws IOException {
+    	String email = (String) httpSession.getAttribute("email");
     	UserDto user = userService.findUserByEmail(email);
+    	DiaryDto diary = new DiaryDto();
     	diary.setUid(user.getId());
+    	diary.setGitName(gitName);
+    	diary.setTitle(title);
+    	diary.setIntro(intro);
+        diary.setGitUrl(gitUrl);
+        diary.setIsProj(isProj);
+        diary.setSDate(sDate);
+        diary.setEDate(eDate);
+
+        if (file != null) {
+            long timestamp = System.currentTimeMillis();
+            StringBuilder builder = new StringBuilder(staticPath);
+            String extension = FilenameUtils.getExtension(file.getOriginalFilename());
+            String fileName = timestamp + "." + extension;
+
+            file.transferTo(new File(builder.append("/")
+                    .append(fileName)
+                    .toString()));
+            diary.setImg(new StringBuilder(BE_BASE_URL)
+                    .append("/diaries/image/")
+                    .append(fileName)
+                    .toString());
+        }
+
     	System.out.println(diary);
     	diaryService.createDiary(diary);
     	return new ResponseEntity<>(diary.getId(), HttpStatus.OK);
@@ -64,13 +110,49 @@ public class DiaryController {
     @Auth
     @PutMapping("/diaries")
     @ApiOperation(value = "다이어리 수정")
-    public Object updateDiary(HttpSession session, @RequestBody DiaryDto diary) {
-    	String email = (String) session.getAttribute("email");
+    public Object updateDiary(HttpSession httpSession,
+                              @RequestParam(required = false) MultipartFile file,
+                              @RequestParam int id,
+                              @RequestParam int uid,
+                              @RequestParam String gitName,
+                              @RequestParam String title,
+                              @RequestParam String intro,
+                              @RequestParam String img,
+                              @RequestParam String gitUrl,
+                              @RequestParam int isProj,
+                              @RequestParam Date sDate,
+                              @RequestParam Date eDate
+                              ) throws IOException {
+    	String email = (String) httpSession.getAttribute("email");
     	UserDto user = userService.findUserByEmail(email);
-    	
-    	int id = diary.getId();
+
     	if(diaryService.getUidById(id) == user.getId()) {
-    		diaryService.updateDiary(diary);
+    	    DiaryDto diary = new DiaryDto();
+            diary.setUid(user.getId());
+            diary.setGitName(gitName);
+            diary.setTitle(title);
+            diary.setIntro(intro);
+            diary.setGitUrl(gitUrl);
+            diary.setIsProj(isProj);
+            diary.setSDate(sDate);
+            diary.setEDate(eDate);
+
+            if (file != null) {
+                long timestamp = System.currentTimeMillis();
+                StringBuilder builder = new StringBuilder(staticPath);
+                String extension = FilenameUtils.getExtension(file.getOriginalFilename());
+                String fileName = timestamp + "." + extension;
+
+                file.transferTo(new File(builder.append("/")
+                        .append(fileName)
+                        .toString()));
+                diary.setImg(new StringBuilder(BE_BASE_URL)
+                        .append("/diaries/image/")
+                        .append(fileName)
+                        .toString());
+            }
+
+            diaryService.updateDiary(diary);
     		return new ResponseEntity<>(HttpStatus.OK);
     	}else {
     		return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
@@ -91,6 +173,18 @@ public class DiaryController {
     	}else {
     		return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
     	}
+    }
+
+    @GetMapping("/diaries/image/{fileName}")
+    public ResponseEntity<Resource> getImage(@PathVariable String fileName) throws IOException {
+        Path path = Paths.get(staticPath + "/" + fileName);
+        String contentType = Files.probeContentType(path);
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.add(HttpHeaders.CONTENT_TYPE, contentType);
+
+        Resource resource = new InputStreamResource(Files.newInputStream(path));
+        return new ResponseEntity<>(resource, headers, HttpStatus.OK);
     }
 }
 
