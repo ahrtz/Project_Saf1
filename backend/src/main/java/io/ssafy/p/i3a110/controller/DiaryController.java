b271258af42604fb8904d52c3395887fc467d15d
Jihwan Oh <ojhoshlmw@gmail.com>
2020-08-13 17:01:45 +0900

    diarymain language 정보 추가, [S03P13A110-7]

--- backend/src/main/java/io/ssafy/p/i3a110/controller/DiaryController.java
+++ backend/src/main/java/io/ssafy/p/i3a110/controller/DiaryController.java
@@ -8,7 +8,6 @@ import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.HashMap;
-import java.util.Iterator;
 import java.util.List;
 import java.util.StringTokenizer;
 
