4119b538ab3552da8b9d32ded75fcdc3169256a2
chaeeun kim <fairy037@naver.com>
2020-08-14 12:54:51 +0900

    [Add & Fix] status calendar 디자인 수정, Blog Detail UI 상세 수정, UI 틀어진 것 조금씩 수정

--- frontend/src/component/Status.vue
+++ frontend/src/component/Status.vue
@@ -1,48 +1,43 @@
 <template>
-  <div class="d-flex flex-column status-board">
-    <div class="d-flex">
+  <div class="d-flex flex-column status">
+    <div class="d-flex justify-center status-title">
+      <v-icon size="20">date_range</v-icon>&nbsp;Status Calendar
+    </div>
+    <div class="d-flex justify-center status-container">
       <div v-for="(t, ti) in 3" :key="ti" class="d-flex flex-grow-0">
         <div class="d-flex flex-column">
-          <div
-            v-for="(r, ri) in 4"
-            :key="ri"
-            class="status-board-tile-container d-flex flex-grow-0"
-          >
+          <div v-for="(r, ri) in 4" :key="ri" class="status-tile-container d-flex flex-grow-0">
             <div
               v-for="(c, ci) in dres.slice((ri+ti*4) * 7, (ri+ti*4) * 7 + 7)"
               :key="`ci-${ci}`"
-              class="d-flex flex-grow-0 align-center justify-center status-board-tile"
+              class="d-flex flex-grow-0 align-center justify-center status-tile status-level0"
               :class="{
-            'status-board-level0': c == 0,
-            'status-board-level1': c >= 1 && c <= 2,
-            'status-board-level2': c >= 3 && c <= 4,
-            'status-board-level3': c >= 5 && c <= 6,
-            'status-board-level4': c >= 7,
+            'status-level1': c >= 1 && c <= 2,
+            'status-level2': c >= 3 && c <= 4,
+            'status-level3': c >= 5 && c <= 6,
+            'status-level4': c >= 7,
         }"
             ></div>
             <div
               v-for="(i, ii) in res.slice((ri+ti*4) * 7, (ri+ti*4) * 7 + 7)"
               :key="`ii-${ii}`"
-              class="status-board-tile-img"
+              class="status-tile-img"
             >
+              <img v-if="isProject && i >= 1" style="height: 36px" src="/static/images/sprout.png" />
               <img
-                v-if="type == 'project' && i >= 1"
-                style="height: 36px"
-                src="/static/images/sprout.png"
-              />
-              <img
-                v-if="type == 'blog' && i >= 1"
+                v-if="!isProject && i >= 1"
                 style="height: 24px;opacity:0.7;"
                 src="/static/images/flower.png"
               />
               <img
-                v-if="type == 'all' && i >= 1"
+                v-if="isProject == null && i >= 1"
                 style="height: 28px;opacity:0.6"
                 src="/static/images/tree.png"
               />
             </div>
           </div>
         </div>
+        <div v-if="ti != 2" class="d-flex flex-grow-0 status-div" />
       </div>
     </div>
     <div class="d-flex">
@@ -65,13 +60,99 @@ export default {
     uid: 0,
     did: 0,
     repoId: '',
-    type: '',
+    isProject: null,
   },
   data: () => ({
     dreq: {},
     dres: [],
     req: {},
     res: [],
+    default: [
+      0,
+      0,
+      0,
+      0,
+      0,
+      0,
+      0,
+      0,
+      0,
+      0,
+      0,
+      0,
+      0,
+      0,
+      0,
+      0,
+      0,
+      0,
+      0,
+      0,
+      0,
+      0,
+      0,
+      0,
+      0,
+      0,
+      0,
+      0,
+      0,
+      0,
+      0,
+      0,
+      0,
+      0,
+      0,
+      0,
+      0,
+      0,
+      0,
+      0,
+      0,
+      0,
+      0,
+      0,
+      0,
+      0,
+      0,
+      0,
+      0,
+      0,
+      0,
+      0,
+      0,
+      0,
+      0,
+      0,
+      0,
+      0,
+      0,
+      0,
+      0,
+      0,
+      0,
+      0,
+      0,
+      0,
+      0,
+      0,
+      0,
+      0,
+      0,
+      0,
+      0,
+      0,
+      0,
+      0,
+      0,
+      0,
+      0,
+      0,
+      0,
+      0,
+      0,
+      0,
+    ],
   }),
   methods: {
     async getDStatus() {
@@ -99,50 +180,69 @@ export default {
     this.dreq.uid = this.req.uid = this.uid;
     this.req.did = this.did;
     this.dreq.repoId = this.repoId;
-    this.getDStatus();
+    if (this.isProject) {
+      this.getDStatus();
+    }
     this.getStatus();
   },
 };
 </script>
 
 <style>
-.status-board {
+.status {
   width: 100%;
   padding-bottom: 40px;
 }
 
-.status-board-tile-container {
+.status-container {
+  width: 100%;
+}
+
+.status-title {
+  padding-top: 8px;
+  font-size: 18px;
+  font-weight: 600;
+  margin-bottom: 8px;
+  color: rgb(0, 0, 0, 0.54);
+}
+
+.status-div {
+  margin: 0 8px;
+  border: dashed 2px #ddd;
+}
+
+.status-tile-container {
   position: relative;
 }
 
-.status-board-tile {
-  width: 36px;
-  height: 36px;
+.status-tile {
+  width: 35px;
+  height: 35px;
   border-radius: 4px;
-  margin: 2px;
+  margin: 1.5px;
 }
 
-.status-board-level0 {
+.status-level0 {
   background-color: rgba(27, 31, 35, 0.04);
 }
 
-.status-board-level1 {
-  background-color: rgba(169, 230, 154, 0.2);
+.status-level1 {
+  background-color: rgba(155, 233, 168, 0.8);
 }
 
-.status-board-level2 {
-  background-color: rgba(169, 230, 154, 0.3);
+.status-level2 {
+  background-color: rgba(64, 196, 99, 0.8);
 }
 
-.status-board-level3 {
-  background-color: rgba(169, 230, 154, 0.4);
+.status-level3 {
+  background-color: rgba(48, 161, 78, 0.8);
 }
 
-.status-board-level4 {
-  background-color: rgba(169, 230, 154, 0.5);
+.status-level4 {
+  background-color: rgba(33, 110, 57, 0.8);
 }
 
-.status-board-tile-img {
+.status-tile-img {
   position: absolute;
 }
 </style>
