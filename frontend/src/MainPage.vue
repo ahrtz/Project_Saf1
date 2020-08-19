47c56c5fff25cd54a1d1f299929271019599bb92
chaeeun kim <fairy037@naver.com>
2020-08-19 15:44:02 +0900

    [Fix] main page tag UI 깨지는 것 수정

--- frontend/src/MainPage.vue
+++ frontend/src/MainPage.vue
@@ -11,7 +11,10 @@
           <Status :uid="uid" :isProj="2" />
           <div class="d-flex">
             <div class="d-flex">
-              <div class="d-flex flex-column flex-shrink-0" style="width: 100%;padding-right:32px">
+              <div
+                class="d-flex flex-column flex-grow-0 flex-shrink-0"
+                style="width: 404px;margin-right:32px"
+              >
                 <div
                   class="d-flex align-center flex-grow-0"
                   style="padding-bottom: 8px;margin-bottom:16px; border-bottom:solid 1px #dde3ea"
@@ -24,43 +27,38 @@
                     @click="$router.push({name:'DiaryMain',params:{uid:uid,test:1}})"
                   >more projects</div>
                 </div>
-                <div class="main-page-card" v-for="(post,indexs) in list_proj" :key="post.id">
+                <div
+                  class="d-flex flex-column flex-grow-0 main-page-card"
+                  v-for="(post,indexs) in list_proj"
+                  :key="post.id"
+                >
                   <!-- card layout -->
-                  <div>
-                    <!-- 프로필 이미지, 닉네임  -->
-                    <div class="d-flex align-center main-card-header">
-                      <img :src="post.userinfo.img" alt class="main-card-header-img" />
-                      <div class="main-card-header-nick_date">
-                        <div class="main-page-nickname">{{post.userinfo.nickname}}</div>
-                        <div class="main-page-cdate">{{post.cdate}}</div>
-                      </div>
-                        <v-icon class="d-flex justify-end ">mdi-source-commit</v-icon>
-                        <span class="main-page-commitcnt">{{post.commitCnt}}</span>
+                  <!-- 프로필 이미지, 닉네임  -->
+                  <div class="d-flex align-center main-card-header">
+                    <img :src="post.userinfo.img" alt class="main-card-header-img" />
+                    <div class="main-card-header-nick_date">
+                      <div class="main-page-nickname">{{post.userinfo.nickname}}</div>
+                      <div class="main-page-cdate">{{post.cdate}}</div>
                     </div>
-                    <!-- 포스트 제목 / 컨텐츠 -->
+                    <v-icon class="d-flex justify-end">mdi-source-commit</v-icon>
+                    <span class="main-page-commitcnt">{{post.commitCnt}}</span>
+                  </div>
+                  <!-- 포스트 제목 / 컨텐츠 -->
+                  <div
+                    class="main-card-article"
+                    @click="$router.push({name:'PostDetail',params:{pid:post.id}})"
+                    style="cursor:pointer"
+                  >
+                    <div class="main-page-content-title">{{post.title}}</div>
+                    <div class="main-page-content-text" v-html="compiledMarkdown(post)"></div>
+                  </div>
+
+                  <div class="d-flex main-page-tag-container">
                     <div
-                      class="main-card-article"
-                      @click="$router.push({name:'PostDetail',params:{pid:post.id}})"
-                      style="cursor:pointer"
-                    >
-
-                      
-
-                      <div class="main-page-content-title">{{post.title}}</div>
-                      <div class="main-page-content-text" v-html="compiledMarkdown(post)"></div>
-                    </div>
-
-                    <footer>
-                      <div >
-                        <div
-                          class="d-flex flex-grow-0 postss-detail-tag"
-                          v-for="(tags,index) in tag_proj[indexs]"
-                          :key="'t-'+index"
-                        >
-                            #{{tags.name}}
-                        </div>
-                      </div>
-                    </footer>
+                      class="d-flex flex-grow-0 main-page-tag"
+                      v-for="(tags,index) in tag_proj[indexs]"
+                      :key="'t-'+index"
+                    >#{{tags.name}}</div>
                   </div>
                 </div>
                 <infinite-loading
@@ -72,7 +70,7 @@
               </div>
             </div>
             <div class="d-flex">
-              <div class="d-flex flex-column flex-shrink-0" style="width: 100%;">
+              <div class="d-flex flex-column flex-shrink-0" style="width: 404px;">
                 <div
                   class="d-flex align-center flex-grow-0"
                   style="padding-bottom: 8px;margin-bottom:16px; border-bottom:solid 1px #dde3ea"
@@ -96,27 +94,21 @@
                       </div>
                     </div>
                     <!-- 포스트 제목 / 컨텐츠 -->
-                      <div
-                        class="main-card-article"
-                        @click="$router.push({name:'PostDetail',params:{pid:post.id}})"
-                        style="cursor:pointer"
-                      >
-                      
-
+                    <div
+                      class="main-card-article"
+                      @click="$router.push({name:'PostDetail',params:{pid:post.id}})"
+                      style="cursor:pointer"
+                    >
                       <div class="main-page-content-title">{{post.title}}</div>
                       <div class="main-page-content-text" v-html="compiledMarkdown(post)"></div>
                     </div>
-                    <footer>
-                      <div >
-                        <div
-                          class="d-flex flex-grow-0 postss-detail-tag"
-                          v-for="(tags,index) in tag_blog[indexss]"
-                          :key="'t-'+index"
-                        >
-                            #{{tags.name}}
-                        </div>
-                      </div>
-                    </footer>
+                    <div class="d-flex main-page-tag-container">
+                      <div
+                        class="d-flex flex-grow-0 main-page-tag"
+                        v-for="(tags,index) in tag_blog[indexss]"
+                        :key="'t-'+index"
+                      >#{{tags.name}}</div>
+                    </div>
                   </div>
                 </div>
                 <infinite-loading
@@ -139,7 +131,7 @@ import axios from 'axios';
 import Status from '@/component/Status.vue';
 import InfiniteLoading from 'vue-infinite-loading';
 import SContact from '@/component/s-contact.vue';
-import marked from 'marked'
+import marked from 'marked';
 
 var renderer = new marked.Renderer();
 
@@ -154,7 +146,7 @@ export default {
   data() {
     return {
       status: ['날짜정보 혹은 뭐 커밋 정보'],
-      
+
       blog_posts: {},
       project_posts: {},
       user: {},
@@ -165,27 +157,24 @@ export default {
       list_blog: [],
       isLogin: false,
       uid: '',
-      tag_proj:{},
-      tag_blog:{}
+      tag_proj: {},
+      tag_blog: {},
     };
   },
   created() {
     this.isLogin = this.$store.state.isLoggedIn;
     this.uid = this.$route.params.uid;
-
-
-    
   },
   methods: {
     compiledMarkdown: function (posttmp) {
-          let vm = posttmp
-          console.log(vm)
-          renderer.em = function(text) {
-        return '<em>' + "" + '</em>';
-      }
-      var tmp1 = marked(posttmp.content, { renderer: renderer }); 
-      
-      return tmp1
+      let vm = posttmp;
+      console.log(vm);
+      renderer.em = function (text) {
+        return '<em>' + '' + '</em>';
+      };
+      var tmp1 = marked(posttmp.content, { renderer: renderer });
+
+      return tmp1;
     },
 
     async infiniteHandler($state) {
@@ -199,21 +188,21 @@ export default {
           limit: this.limit_proj + 10,
         })
         .then((res) => {
-          setTimeout(async() => {
+          setTimeout(async () => {
             if (res.data.length >= this.limit_proj) {
               this.list_proj = res.data;
-              console.log(res.data[1])
-              for (var i=0;i<res.data.length;i++){
-                this.tag_proj[i]=await this.$api.tagIndex(res.data[i].id)
+              console.log(res.data[1]);
+              for (var i = 0; i < res.data.length; i++) {
+                this.tag_proj[i] = await this.$api.tagIndex(res.data[i].id);
               }
-              
+
               $state.loaded();
               this.limit_proj += 10;
             } else {
               $state.complete();
             }
           }, 1000);
-          this.$forceUpdate()
+          this.$forceUpdate();
         })
         .catch((err) => {
           console.log(err);
@@ -233,8 +222,8 @@ export default {
           setTimeout(async () => {
             if (res.data.length >= this.limit_blog) {
               this.list_blog = res.data;
-              for (var i=0;i<res.data.length;i++){
-                this.tag_blog[i]=await this.$api.tagIndex(res.data[i].id)
+              for (var i = 0; i < res.data.length; i++) {
+                this.tag_blog[i] = await this.$api.tagIndex(res.data[i].id);
               }
 
               $state.loaded();
@@ -243,7 +232,7 @@ export default {
               $state.complete();
             }
           }, 1000);
-          this.$forceUpdate()
+          this.$forceUpdate();
         })
         .catch((err) => {
           console.log(err);
@@ -262,10 +251,9 @@ export default {
   height: 55px;
   width: 100%;
 }
-.main-page-commitcnt{
-  font-size: 14px
+.main-page-commitcnt {
+  font-size: 14px;
 }
-
 
 .main-card-header-nick_date {
   display: flex;
@@ -360,7 +348,13 @@ export default {
   font-size: 12px;
 }
 
-.postss-detail-tag {
+.main-page-tag-container {
+  width: 100%;
+  overflow: hidden;
+}
+
+.main-page-tag {
+  white-space: nowrap;
   margin-bottom: 8px;
   margin-right: 8px;
   padding: 0 12px;
