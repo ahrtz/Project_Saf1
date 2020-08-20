67ce2b0a46b1b310a4137314af098524f31227f3
chaeeun kim <fairy037@naver.com>
2020-08-20 16:48:45 +0900

    [Fix] 전체 letter spacing 줄이기, group UI 디테일 수정

--- frontend/src/pages/Group/GroupDetail.vue
+++ frontend/src/pages/Group/GroupDetail.vue
@@ -46,7 +46,7 @@
 
           <v-row style="width: 768px;margin:0 auto;">
             <!-- 그룹 이름 & 그룹 디테일 -->
-            <v-col class="group-detail-ino-box">
+            <v-col class="group-detail-info-box">
                 <div class="group-detail-info-title">그룹 정보</div>
               <div class="group-detail-info-name">{{ group_info.name }}</div>
               <div class="group-detail-info-content">{{ group_info.intro }}</div>
@@ -77,20 +77,22 @@
             </v-col>
           </v-row>
           <v-row class="flex-column" style="width: 768px;margin:0 auto;margin-top: 32px;">
-              <div class="d-flex align-center justify-center">
-                  <img style="width: 100px" src="/static/images/crown.png"/>
+              <div class="d-flex align-center justify-center" style="padding-bottom: 16px;">
+                  <img style="width: 30px" src="/static/images/crown.png"/>
+                  <div class="group-detail-rank-title">Rainking Top 5</div>
+                  <img style="width: 30px" src="/static/images/crown.png"/>
               </div>
               <div class="d-flex">
             <!-- 순위 리스트 3개 -->
               <!-- 좋아요 순위(Top 3) -->
               <v-col class="group-detail-info-box">
                 <v-list subheader>
-                  <v-subheader style="border-bottom: solid 1px #dde3ea;margin-bottom:8px">좋아요 Top 5</v-subheader>
+                  <v-subheader class="group-detail-rank-subtitle" style="border-bottom: solid 1px #dde3ea;margin-bottom:8px">좋아요 Top 5</v-subheader>
 
                   <v-list-item
                     v-for="like_item in lRateList"
                     :key="like_item.nickname"
-                    style="cursor:pointer"
+                    style="cursor:pointer;"
                     @click="$router.push({ name: 'MainPagefor', params: { uid: like_item.uid } })"
                   >
                     <v-list-item-avatar>
@@ -98,10 +100,10 @@
                         :src="like_item.img == null ? '/static/images/user.png' : like_item.img"
                       ></v-img>
                     </v-list-item-avatar>
-                    <v-list-item-title>
+                    <v-list-item-title style="padding-left:8px;">
                       {{like_item.nickname}}
                       <v-spacer></v-spacer>
-                      {{like_item.cnt}}
+                      {{like_item.cnt}}개
                     </v-list-item-title>
                     <v-list-item-content></v-list-item-content>
                   </v-list-item>
@@ -111,7 +113,7 @@
               <!-- 팔로워 순위(Top 3)  -->
               <v-col class="group-detail-info-box">
                 <v-list subheader>
-                  <v-subheader style="border-bottom: solid 1px #dde3ea;margin-bottom:8px">팔로우 Top 5</v-subheader>
+                  <v-subheader class="group-detail-rank-subtitle" style="border-bottom: solid 1px #dde3ea;margin-bottom:8px">팔로우 Top 5</v-subheader>
 
                   <v-list-item
                     v-for="follower_item in fRateList"
@@ -124,10 +126,10 @@
                         :src="follower_item.img == null ? '/static/images/user.png' : follower_item.img"
                       ></v-img>
                     </v-list-item-avatar>
-                    <v-list-item-title>
+                    <v-list-item-title style="padding-left:8px;">
                       {{follower_item.nickname}}
                       <v-spacer></v-spacer>
-                      {{follower_item.cnt}}
+                      {{follower_item.cnt}}명
                     </v-list-item-title>
                     <v-list-item-content></v-list-item-content>
                   </v-list-item>
@@ -137,7 +139,7 @@
               <!-- 포스트 순위(Top 3) -->
               <v-col class="group-detail-info-box">
                 <v-list subheader>
-                  <v-subheader style="border-bottom: solid 1px #dde3ea;margin-bottom:8px">포스트 Top 5</v-subheader>
+                  <v-subheader class="group-detail-rank-subtitle" style="border-bottom: solid 1px #dde3ea;margin-bottom:8px">포스트 Top 5</v-subheader>
                   <v-list-item
                     v-for="post_item in pRateList"
                     :key="post_item.nickname"
@@ -150,10 +152,10 @@
                         :src="post_item.img == null ? '/static/images/user.png' : post_item.img"
                       ></v-img>
                     </v-list-item-avatar>
-                    <v-list-item-title>
+                    <v-list-item-title style="padding-left:8px;">
                       {{post_item.nickname}}
                       <v-spacer></v-spacer>
-                      {{post_item.cnt}}
+                      {{post_item.cnt}}개
                     </v-list-item-title>
                     <v-list-item-content></v-list-item-content>
                   </v-list-item>
@@ -452,5 +454,17 @@ export default {
 .group-detail-info-content {
     margin-top: 8px;
   font-size: 12px;
+}
+
+.group-detail-rank-title {
+  font-size: 20px;
+  padding: 0 8px;
+  font-weight: 600;
+  letter-spacing: -1.2px;
+}
+
+.group-detail-rank-subtitle {
+  font-size: 14px;
+  font-weight: 600;
 }
 </style>
